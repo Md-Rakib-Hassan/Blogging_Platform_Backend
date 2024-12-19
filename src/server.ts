1bb9eb36effb91aeb66eb1af5a92003b1b12b69f
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2024-12-19 23:36:50 +0600

    Connected the database and write the server code

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./app/config";
+
+async function main() {
+    await mongoose.connect(config.database_uri as string);
+}
+
+main();
+
+app.listen(config.port, () => {
+    console.log(`server is running at ${config.port}`);
+})
