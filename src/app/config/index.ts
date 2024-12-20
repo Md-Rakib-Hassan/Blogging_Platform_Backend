2cdc8498a28938c67d6d4f83bdd4871a483961a1
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2024-12-21 01:49:48 +0600

    login completed

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -4,5 +4,7 @@ import path from 'path';
 dotenv.config({ path: path.join(process.cwd(), '.env') });
 export default {
     port: process.env.PORT,
-    database_uri:process.env.DATABASE_URI
+    database_uri: process.env.DATABASE_URI,
+    jwt_secret: process.env.JWT_SECRET,
+    jwt_expiration: process.env.JWT_EXPIRES_IN,
 }
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,23 +1,31 @@
-import AppError from "../../errors/AppError";
-import { IUser } from "./user.interface";
-import UserModel from "./user.model";
+import AppError from '../../errors/AppError';
+import { IUser } from './user.interface';
+import UserModel from './user.model';
 
 const createUserIntoDB = async (userData: IUser) => {
-    const isUserExist = await getSingleUserFromDB(userData.email);
-    if (isUserExist) {
-        throw new AppError(400, 'User already exists');
+    try {
+        const isUserExist = await getSingleUserFromDB(userData.email);
+        if (isUserExist) {
+            throw new AppError(400, 'User already exists');
+        }
+        const createdUser = await UserModel.create(userData);
+        const result = {
+            _id: createdUser._id,
+            name: createdUser.name,
+            email: createdUser.email,
+        };
+        return result;
+    } catch (err) { 
+        throw new AppError(401, 'Invalid credentials');
     }
-    const result = await UserModel.create(userData);
-    return result;
-}
+};
 
 const getSingleUserFromDB = async (email: string) => {
-    const result = await UserModel.findOne({email});
-    return result;
-} 
-
+  const result = await UserModel.findOne({ email });
+  return result;
+};
 
 export const UserService = {
-    createUserIntoDB,
-    getSingleUserFromDB
-}
+  createUserIntoDB,
+  getSingleUserFromDB,
+};
